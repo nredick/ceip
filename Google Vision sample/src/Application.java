459afe4a0b21de02b5131548fb03156a23c9b9e7
Nathalie Redick <nathalieredick@gmail.com>
2019-05-21 21:42:42 -0400

    ds store fix

--- Google Vision sample/src/Application.java
+++ Google Vision sample/src/Application.java
@@ -30,38 +30,40 @@ public class Application {
             int count = 1;
             if(listings != null) {
                 for (File f : listings) {
-                    System.out.println("\nImage #" + count + ": \n");
-                    count++;
-                    Path path = Paths.get(f.toString());
-                    byte[] data = Files.readAllBytes(path);
-                    ByteString imgBytes = ByteString.copyFrom(data);
+                    if(!(f.toString().contains("/."))){
+                        System.out.println("\nImage #" + count + ": \n");
+                        count++;
+                        Path path = Paths.get(f.toString());
+                        byte[] data = Files.readAllBytes(path);
+                        ByteString imgBytes = ByteString.copyFrom(data);
 
-                    // Builds the image annotation request
-                    List<AnnotateImageRequest> requests = new ArrayList<>();
-                    Image img = Image.newBuilder().setContent(imgBytes).build();
-                    Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
-                    AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
-                            .addFeatures(feat)
-                            .setImage(img)
-                            .build();
-                    requests.add(request);
+                        // Builds the image annotation request
+                        List<AnnotateImageRequest> requests = new ArrayList<>();
+                        Image img = Image.newBuilder().setContent(imgBytes).build();
+                        Feature feat = Feature.newBuilder().setType(Type.LABEL_DETECTION).build();
+                        AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                                .addFeatures(feat)
+                                .setImage(img)
+                                .build();
+                        requests.add(request);
 
-                    // Performs label detection on the image file
-                    BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
-                    List<AnnotateImageResponse> responses = response.getResponsesList();
+                        // Performs label detection on the image file
+                        BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+                        List<AnnotateImageResponse> responses = response.getResponsesList();
 
-                    for (AnnotateImageResponse res : responses) {
-                        if (res.hasError()) {
-                            System.out.printf("Error: %s\n", res.getError().getMessage());
-                            return;
+                        for (AnnotateImageResponse res : responses) {
+                            if (res.hasError()) {
+                                System.out.printf("Error: %s\n", res.getError().getMessage());
+                                return;
+                            }
+
+                            for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+                                annotation.getAllFields().forEach((k, v) ->
+                                        System.out.printf("%s : %s\n", k, v.toString()));
+                            }
                         }
 
-                        for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
-                            annotation.getAllFields().forEach((k, v) ->
-                                    System.out.printf("%s : %s\n", k, v.toString()));
-                        }
                     }
-
                 }
             }
 
